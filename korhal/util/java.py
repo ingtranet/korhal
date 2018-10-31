3684a911ee162dd2800e6b6bf21ae2557ba4eacb
cookieshake <cookieshake.dev@gmail.com>
2018-11-01 00:02:12 +0900

    fix for support windows

--- korhal/util/java.py
+++ korhal/util/java.py
@@ -7,25 +7,25 @@ from korhal import install_path
 
 def init_server():
     if os.getenv('JAVA_HOME'):
-        run_command = '{} -jar {}'.format(
-            os.path.join(os.getenv('JAVA_HOME'), 'bin', 'java'),
-            os.path.join(install_path, 'korhal-java-server-0.1.1.jar')
-        )
+        java_path = os.path.join(os.getenv('JAVA_HOME'), 'bin', 'java')
     else:
-        run_command = 'java -jar {}'.format(os.path.join(install_path, 'korhal-java-server-0.1.1.jar'))
-
-    args = shlex.split(run_command)
-    server = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        java_path = 'java'
+    jar_path = os.path.join(install_path, 'korhal-java-server-0.1.1.jar')
+    
+    args = [java_path, '-jar', jar_path]
+    server = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     def _call_at_exit():
         server.kill()
         server.wait()
     atexit.register(_call_at_exit)
     
     while server.poll() is None:
-        line = server.stderr.readline().decode()
-        if 'Server started' in line:
+        line = server.stderr.readline()
+        if b'Server started' in line:
             break
 
     if server.poll() is not None:
         stdout, stderr = server.communicate()
         raise Exception(stdout + stderr)
+
+
